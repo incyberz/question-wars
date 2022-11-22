f7292753459961eda30f9f490faa3c98648b8100
incyberz <incyberz@yahoo.com>
2022-11-22 12:30:36 +0700

    Update List Challenger

---
+++ docs/chal_p13_php_form.php
@@ -0,0 +1,31 @@
+<link rel='stylesheet' type='text/css' href='../assets/vendor/bootstrap/css/bootstrap.css'>
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<?php
+$p_before = 'P07';
+$p_ke = 'p06-spa-github-pages';
+?>
+
+<h4>CRUD Challenge with PHP-HTML Form</h4>
+
+<p>Praktikan kembali dirumah Video Perkuliahan P13 PHP-HTML Form tentang Membuat Aplikasi CRUD Sederhana dengan PHP MySQL!</p>
+
+<div class="alert alert-danger">Warning! Bukan video demo aplikasi, tapi yang diminta adalah rekaman saat kamu mengetikan kode yang kamu buat.</div>
+<p>Note: <code>Challenge ini opsional untuk menambah bekal karirmu atau sebagai portfolio system yang pernah kamu buat. </code></p>
+
+
+<h5>Langkah Pengerjaan:</h5>
+<ol>
+  <!-- <li>Silahkan subscribe dulu <a href="https://www.youtube.com/channel/UCjS_z5jUZJd_YnkZvFPZsfw" target="_blank">Channel Youtube Dosen</a>!</li> -->
+  <li>Siapkan laptop dan aplikasi perekam layar semisal <a href="https://www.google.com/search?client=firefox-b-d&q=camtasia" target="_blank">Camtasia</a>, dll</li>
+  <li>Lihat tutorial <a href="https://youtu.be/Pdhta3OzTPU" target="_blank">Membuat Aplikasi CRUD Sederhana dengan PHP MySQL</a></li>
+  <li>Ikuti tutorial hingga selesai!</li>
+  <li>Silahkan rekaman sambil kamu membuat aplikasi yang ingin kamu buat (boleh tanpa suara)</li>
+  <li>Percepat (<code>fast-forward</code>) hasil rekaman kamu menjadi sekitar 2 menit | lihat <a href="https://www.google.com/search?client=firefox-b-d&q=cara+fast+forward+video+di+camtasia" target="_blank">cara mempercepat video</a></li>
+  <li>Buatlah <code>video intro</code> perkenalan diri, terlihat wajah, sebutkan NIM, Nama, kelas. Intro example: Halo kawan2! saya Ahmad Firdaus, NIM 41414141, kelas TI-2025-REG-SORE akan menampilkan bukti challenge saya tentang [...]. Berikut videonya!</li>
+  <li>Buatlah <code>video outro</code>! Outro example: itulah video rekamannnya sebagai bukti bahwa saya telah praktikum mandiri dirumah. Terimakasih kepada [dosen, kawan] yang telah [...]. Sampai jumpa!</li>
+  <li>Gabungkan video tersebut (<a href="https://www.google.com/search?client=firefox-b-d&q=video+joiner" target="_blank"><code>join-video</code></a>) dan upload ke Youtube!</li>
+  <li>Submit Challenge!</li>
+</ol>
+
+<div class="text-center alert alert-success"><q>Tak usah mencari, sibuklah membekali diri, biar saat melamar percaya diri! Sapa bilang mencari kerja itu sulit, para hirer justru lebih sulit menemukan Anda wahai para Skiller! Happy coding!</q></div>
+
